625fdd2de708c5a98d33be9e5ff12f957ed2088a
四喜 <84219304+3ixi@users.noreply.github.com>
2025-12-02 11:06:25 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -72,7 +72,7 @@ push_config = {
     'CHAT_URL': '',                     # synology chat url
     'CHAT_TOKEN': '',                   # synology chat token
 
-    'PUSH_PLUS_TOKEN': 'd6b3e3782b0542018dda76a1940a521f',              # push+ 微信推送的用户令牌
+    'PUSH_PLUS_TOKEN': '',              # push+ 微信推送的用户令牌
     'PUSH_PLUS_USER': '',               # push+ 微信推送的群组编码
 
     'WE_PLUS_BOT_TOKEN': '',            # 微加机器人的用户令牌
@@ -978,3 +978,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
